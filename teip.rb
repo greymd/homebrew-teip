f7263d39e00ccc88ec75ba1b261692d2fc746708
Yasuhiro Yamada <yamada@gr3.ie>
2023-03-11 14:37:20 +0000

    teip: v2.3.0

--- teip.rb
+++ teip.rb
@@ -1,15 +1,14 @@
 class Teip < Formula
   desc "Masking tape to help commands \"do one thing well\""
   homepage "https://github.com/greymd/teip"
-  url "https://github.com/greymd/teip/archive/v2.2.0.tar.gz"
-  sha256 "11816abcecaebc8aa1dc8131387baa4030ba7e9f48994733c091b04a2c992381"
+  url "https://github.com/greymd/teip/archive/v2.3.0.tar.gz"
+  sha256 "4c39466613f39d27fa22ae2a6309ac732ea94fdbc8711ecd4149fc1ecdfbaedf"
   license "MIT"
   head "https://github.com/greymd/teip.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    ENV["SHELL_COMPLETIONS_DIR"] = buildpath
     system "cargo", "install", "--features", "oniguruma", *std_cargo_args
     man1.install "man/teip.1"
     zsh_completion.install "completion/zsh/_teip"
@@ -20,7 +19,5 @@ class Teip < Formula
   test do
     ENV["TEIP_HIGHLIGHT"] = "<{}>"
     assert_match /AAA/, pipe_output("#{bin}/teip -c 1", "<A>AA", 0)
-    # Invalid option
-    pipe_output("#{bin}/teip -c 2-1", "test", 1)
   end
 end
